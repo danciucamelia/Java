2c8dc8bb9a4e1caed0feacf3c9f3059097ca4cdd
danciucamelia <92920193+danciucamelia@users.noreply.github.com>
2024-03-10 03:05:05 +0200

    Create ecuatii simple numere double.java

---
+++ ecuatii simple numere double.java
@@ -0,0 +1,21 @@
+
+//Danciu Camelia, gr 2123
+import java.lang.Math;
+public class lab1
+{
+public static void main(String[] args)
+{
+double x,y,z;
+int i;
+i=10;
+y=Math.log(10.0);
+x=Math.pow(3.0,4.0);
+z=Math.random(); // random number from 0 to 1
+System.out.println("Value of i is " + i);
+System.out.println("Value of log(10) is " + y);
+System.out.println("Value of 3 at power 4 is " + x);
+System.out.println("A random number is " + z);
+System.out.println("Square root of 2 is " + Math.sqrt(2));
+System.out.println("media aritmetica a celor 3 numere este:"+(x+y+z)/3);
+}
+}
